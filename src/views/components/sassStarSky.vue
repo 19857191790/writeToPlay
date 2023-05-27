4ea23d7adcde9cb066cd2e620883f0e7305e2c9f
赵剑铖 <11311730+zhaojc-077399@user.noreply.gitee.com>
2023-05-27 13:20:37 +0800

    Changes

---
+++ src/views/components/sassStarSky.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="layer1"></div>
+    <div class="layer2"></div>
+    <div class="layer3"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+
+  @function getShadows($n){
+    $shadows:'#{random(100)}vw #{random(100)}vh #fff';
+    @for $i from 2 through $n{
+      $shadows:'#{$shadows},#{random(100)}vw #{random(100)}vh #fff';
+    }
+    @return unquote($shadows);
+  }
+  $duration:400s;
+  $count:400s;
+  @for $i from 1 through 3{
+    $duration:floor($duration / 2);
+    $count:floor($count / 2);
+    .layer#{$i}{
+      $size:#{$i}px;
+      position: fixed;
+      width: $size;
+      height: $size;
+      border-radius: 50%;
+      background: #f40;
+      left: 0;
+      top: 0;
+      box-shadow: getShadows($count);
+      animation: moveUp $duration linear infinite;
+      &::after{
+        content: '';
+        position: fixed;
+        width: $size;
+        height: $size;
+        left: 0;
+        top: 100vh;
+        border-radius: inherit;
+        box-shadow: inherit;
+      }
+    }
+
+    @keyframes moveUp {
+      100%{transform: translateY(-100vh)}
+    }
+
+  }
+}
+</style>
